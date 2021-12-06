c72abb17d4e6751a80deca2ef1e87d4827c64b86
chuan920826 <quailchuanqiu@hotmail.com>
2021-12-06 08:24:50 +0000

    rename transport.vue to index.vue.

---
+++ index.vue
@@ -0,0 +1,47 @@
+<template>
+ 
+  <van-cell-group inset>
+    <van-field
+      v-model="carNO"
+      readonly
+      label="车牌号"
+      @click="carShow = true"
+    />
+
+  </van-cell-group>
+  <vnp-keyboard
+    v-bind:value="carNO"
+    v-on:input="carNO = $event"
+    v-model:show.sync="carShow"
+  />
+</template>
+
+<script lang="ts">
+
+import { defineComponent, ref } from "vue";
+
+
+import Keyboard from "@/components/vant-number-plate/vnp-keyboard.vue";
+export default defineComponent({
+  name: "index",
+  components: {
+    "vnp-keyboard": Keyboard,
+  },
+  setup() {
+   
+    const carNO = ref("");
+    const carShow = ref(false);
+  
+    
+
+   
+    return {
+     
+      carNO,
+      carShow,
+      
+    };
+  },
+});
+</script>
+
